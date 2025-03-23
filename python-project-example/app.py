abe6b6b059e6643c37aa0bfe785090c836a8697f
gangjoohyeong <bles@kakao.com>
2025-03-23 19:39:01 +0900

    feat: deploy
    - update python project

--- python-project-example/app.py
+++ python-project-example/app.py
@@ -1,12 +1,8 @@
-import numpy as np
+from fastapi import FastAPI
 
-arr1 = np.array([1, 2, 3, 4, 5])
-arr2 = np.array([10, 20, 30, 40, 50])
+app = FastAPI()
 
-sum_arr = arr1 + arr2
-mul_arr = arr1 * arr2
-dot_product = np.dot(arr1, arr2)
 
-print("Sum of two arrays: ", sum_arr)
-print("Multiplication of two arrays: ", mul_arr)
-print("Dot product of two arrays: ", dot_product)
+@app.get("/hello")
+def read_root():
+    return {"message": "Hello, World!"}
